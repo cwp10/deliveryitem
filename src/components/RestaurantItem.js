7d8084dfb5c159824e62884d2388320a863c45b4
cwp10 <cwp10@msn.com>
2018-03-27 14:17:30 +0900

    수량 체크 버튼 추가

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -5,23 +5,33 @@ class RestaurantItem extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      item: props,
+      id: this.props.id,
+      name: this.props.name,
+      price: this.props.price,
       qty: 0
     }
+    this.onClickPlus = this.onClickPlus.bind(this)
+    this.onClickMinus = this.onClickMinus.bind(this)
+  }
+
+  onClickPlus (e) {
+    const newValue = this.state.qty + 1
+    this.setState({qty: newValue})
+  }
+
+  onClickMinus (e) {
+    (this.state.qty > 0) ? this.setState({qty: this.state.qty - 1}) : 0
   }
 
   render() {
     return (
       <div className="restaurant-item">
-        <span>
-          {this.state.item.name}
-        </span>
-        <span>
-          {this.state.item.price}
-        </span>
-        <span>
-          {this.state.qty}
-        </span>
+        <span>음식 : {this.state.name}</span>
+        <span>가격 : {this.state.price} 원</span>
+        
+        <button onClick={this.onClickPlus}>+</button>
+        <span>수량 : {this.state.qty} 개</span>
+        <button onClick={this.onClickMinus}>-</button>
       </div>
     )
   }
