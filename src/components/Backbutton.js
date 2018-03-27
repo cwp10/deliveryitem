b88d75e9d571438bbadad522c9190835a541c50b
cwp10 <cwp10@msn.com>
2018-03-27 13:24:26 +0900

    이미지 위치 변경

---
+++ src/components/Backbutton.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../stylesheets/Backbutton.scss'
+
+class Backbutton extends React.Component {
+  render () {
+    const {history} = this.props
+    const goBack = (e) => {
+      history.goBack()
+    }
+
+    return (
+      <div className="backButton" onClick={goBack}>
+        <img src="assets/img/ui/backbtn.png" />
+      </div>
+    )
+  }
+}
+
+export default Backbutton
