04df5534e38d7e68c8d8771e7a932f220da69509
cwp10 <cwp10@msn.com>
2018-03-27 15:18:02 +0900

    주문 완료 페이지

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom'
 import RestaurantList from './RestaurantList'
 import RestaurantDetail from './RestaurantDetail'
+import OrderPage from './OrderPage'
 import '../../stylesheets/App.scss'
 
 const App = ({ restaurants }) =>
@@ -8,6 +9,7 @@ const App = ({ restaurants }) =>
     <Route exact path="/:id" component={() => (
       <RestaurantDetail list={restaurants} />
     )} />
+    <Route path="/:id/order" component={OrderPage} />
     <Route path="/" component={() => (
       <RestaurantList list={restaurants} />
     )} />
---
+++ src/components/OrderPage.js
@@ -0,0 +1,6 @@
+const OrderPage = () =>
+  <div>
+    주문완료페이지
+  </div>
+
+export default OrderPage
