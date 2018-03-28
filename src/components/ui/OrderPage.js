64dde59ff79c33c80001d7c466d9187b032fe39b
WonPyoung Choi <cwp10@4rivergame.com>
2018-03-28 16:38:31 +0900

    총 금액 표시

--- src/components/ui/OrderPage.js
+++ src/components/ui/OrderPage.js
@@ -8,6 +8,13 @@ import '../../../stylesheets/OrderPage.scss'
 class OrderPage extends React.Component {
   render () {
     const { id, photo, restaurant, description, itemList, history } = this.props
+    let totalprice = 0
+    for (let i = 0; i < itemList.length; i++) {
+      if(itemList[i].qty > 0) {
+        totalprice += itemList[i].qty * itemList[i].price
+      }
+    }
+
     return (
       <div>
         <Topbar history={history}/>
@@ -23,6 +30,12 @@ class OrderPage extends React.Component {
               {...item}
             />
           )}
+
+          
+        </div>
+        <br/>
+        <div>
+          총 금액 : {totalprice} 원
         </div>
         <div className="order-button">
           <OrderStart history={history} />
